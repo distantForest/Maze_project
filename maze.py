fcdde69de455400b115babd41efcdaf349126bf4
distantForest <igor.parchakov@mailfence.com>
2023-02-14 22:09:41 +0100

    initial version

---
+++ maze.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.root_widget = Tk()
+        self.root_widget.title = "Maze"
+        self.canva = Canvas(self.root_widget)
+        self.canva.pack()
+        self.running = False
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        return
+
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running == True:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
+
