e804b3fa251cb5468b670d59b560cfab94a2749b
distantForest <igor.parchakov@mailfence.com>
2023-02-16 23:45:21 +0100

    class point added, class line edded, drawing line in window added

--- maze.py
+++ maze.py
@@ -15,7 +15,10 @@ class Window:
     def redraw(self):
         self.root_widget.update_idletasks()
         self.root_widget.update()
-
+        
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.canva, fill_color)
+                  
     def wait_for_close(self):
         self.running = True
         while self.running == True:
@@ -26,8 +29,35 @@ class Window:
     def close(self):
         self.running = False
 
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_1p = Point(0,0), point_2p = Point(0,0)):
+        self.point_1 = Point(point_1p.x, point_1p.y)
+        self.point_2 = Point(point_2p.x, point_2p.y)
+
+    def draw(self, canva, fill_p = "black"):
+        print(self.point_1.x)
+        canva.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill = fill_p, width = 2
+            )
+        canva.pack()
+        
 def main():
+    
     win = Window(800, 600)
+    line_1 = Line(Point(10,10), Point(20,100))
+    line_2 = Line(Point(20,20), Point(800 - 20,600 - 20))
+    win.draw_line(line_1, "black")
+    win.draw_line(line_2, "red")
+    
     win.wait_for_close()
 
 main()
