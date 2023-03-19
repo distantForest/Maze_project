395a557e289b06c9c2427b7e05e121e49fce6591
igor <igor@localhost.localdomain>
2023-03-19 16:44:29 +0100

    supoort for working without rendering added

--- maze.py
+++ maze.py
@@ -25,7 +25,7 @@ class Window:
         self.root_widget.update()
       
     def draw_line(self, line, fill_color="black"):
-        line.draw(self.canva, fill_color)
+        line.draw(fill_color)
 
     def wait_for_close(self):
         self.running = True
@@ -45,19 +45,21 @@ class Point:
 
        
 class Line:
-    def __init__(self, point_1p=Point(0, 0), point_2p=Point(0, 0)):
+    def __init__(self, point_1p=Point(0, 0), point_2p=Point(0, 0), win=None):
         self.point_1 = Point(point_1p.x, point_1p.y)
         self.point_2 = Point(point_2p.x, point_2p.y)
+        self._win = win
 
-    def draw(self, canva, fill_p="black"):
-        canva.create_line(
-            self.point_1.x,
-            self.point_1.y,
-            self.point_2.x,
-            self.point_2.y,
-            fill=fill_p, width=2
+    def draw(self, fill_p="black"):
+        if self._win is not None:
+            self._win.canva.create_line(
+                self.point_1.x,
+                self.point_1.y,
+                self.point_2.x,
+                self.point_2.y,
+                fill=fill_p, width=2
             )
-        canva.pack()
+            self._win.canva.pack()
 
      
 class Cell:
@@ -78,25 +80,25 @@ class Cell:
         self.center = Point(b_c.x + side_x // 2, b_c.y + side_y // 2)
 
         # Create named tuple of lines
-        self.lines = Lines(left=Line(point_3, point_0),    # left wall   0 -- 1
-                           top=Line(point_0, point_1),     # top wall    |    |
-                           right=Line(point_1, point_2),   # right wall  3 -- 2
-                           bottom=Line(point_2, point_3)   # bottom wall
+        self.lines = Lines(left=Line(point_3, point_0, self.win),    # left wall   0 -- 1
+                           top=Line(point_0, point_1, self.win),     # top wall    |    |
+                           right=Line(point_1, point_2, self.win),   # right wall  3 -- 2
+                           bottom=Line(point_2, point_3, self.win)   # bottom wall
                            )
         return
       
     def draw(self):
         for wall, line in zip(self.walls, self.lines):
             if wall is True:
-                line.draw(self.win.canva)
+                line.draw()
         c_line = Line(Point(self.center.x-5, self.center.y-5),
-                      Point(self.center.x+5, self.center.y+5))
-        c_line.draw(self.win.canva, "green")
+                      Point(self.center.x+5, self.center.y+5), self.win)
+        c_line.draw("green")
         Line(Point(self.center.x-5, self.center.y+5),
-             Point(self.center.x+5, self.center.y-5)).draw(self.win.canva, "green")
+             Point(self.center.x+5, self.center.y-5), self.win).draw("green")
 
     def draw_move(self, to_cell, undo=False):
-        Line(self.center, to_cell.center).draw(self.win.canva, "grey" if undo else "red")
+        Line(self.center, to_cell.center, self.win).draw("grey" if undo else "red")
 
 
 class Maze:
@@ -117,7 +119,7 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._cells = [[True for i in range(self._colls)] for j in range(self._rows)]
+        self._cells = [[True for i in range(self._rows)] for j in range(self._colls)]
         self._create_cells()
 
     def _create_cells(self):
@@ -139,31 +141,33 @@ class Maze:
         return
 
     def _animate(self):
-        self._win.redraw()
-        time.sleep(0.1)
+        if self._win is not None:
+            self._win.redraw()
+            time.sleep(0.1)
     
                
-def main():
-  
-    win = Window(800, 600)
-    line_1 = Line(Point(10, 10), Point(20, 100))
-    line_2 = Line(Point(20, 20), Point(800 - 20, 600 - 20))
-    win.draw_line(line_1, "black")
-    win.draw_line(line_2, "red")
-    cell1 = Cell(win, Point(15, 15), Point(45, 45))
-    cell1.draw()
-    cell2 = Cell(win, Point(50, 15), Point(80, 45), (True, False, True, False))
-    cell2.draw()
-    cell3 = Cell(win, Point(15, 150), Point(45, 195))
-    cell3.draw()
-    cell4 = Cell(win, Point(70, 95), Point(110, 135), (True, False, True, False))
-    cell4.draw()
+if __name__ == "__main__":
+    def main():
+    
+        win = Window(800, 600)
+        line_1 = Line(Point(10, 10), Point(20, 100), win)
+        line_2 = Line(Point(20, 20), Point(800 - 20, 600 - 20), win)
+        win.draw_line(line_1, "black")
+        win.draw_line(line_2, "red")
+        cell1 = Cell(win, Point(15, 15), Point(45, 45))
+        cell1.draw()
+        cell2 = Cell(win, Point(50, 15), Point(80, 45), (True, False, True, False))
+        cell2.draw()
+        cell3 = Cell(win, Point(15, 150), Point(45, 195))
+        cell3.draw()
+        cell4 = Cell(win, Point(70, 95), Point(110, 135), (True, False, True, False))
+        cell4.draw()
+        
+        cell4.draw_move(cell1)
+        cell2.draw_move(cell3,True)
 
-    cell4.draw_move(cell1)
-    cell2.draw_move(cell3,True)
-
-    a  = Maze(1, 1, 10, 10, 24, 24, win)
-    win.wait_for_close()
+        a  = Maze(1, 1, 10, 15, 24, 24, win)
+        win.wait_for_close()
 
    
-main()
+    main()
